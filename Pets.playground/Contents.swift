3bfc062514b73217ecc59137a7faa10807d9923d
salmaanrizvi <sar228@cornell.edu>
2016-07-10 22:42:34 -0400

    ??

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -4,9 +4,7 @@ print(petName)
 
 if petName != nil {
     print("My friend's pet is \(petName)")
-} else {
-    print("My friend doesn't have a pet 😞")
-}
+} else { print("My friend doesn't have a pet 😞") }
 
 if let petName = petName {
     print("My friend's pet is \(petName)")
